28e229e02c2fdb4772ac8c14e3dd57c13b7dcdb8
Gherman <ion.gherman06@gmail.com>
2015-01-08 12:57:26 +0200

    add chat client to project

---
+++ chatClient/src/MultiThreadChatClient.java
@@ -0,0 +1,98 @@
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MultiThreadChatClient implements Runnable {
+
+  //client socket
+  private static Socket clientSocket = null;
+  //output stream
+  private static PrintStream os = null;
+  //input stream
+  private static DataInputStream is = null;
+
+  private static BufferedReader inputLine = null;
+  private static boolean closed = false;
+  
+  public static void main(String[] args) {
+
+    //default port.
+    int portNumber = 2222;
+    //default host.
+    String host = "localhost";
+
+    if (args.length < 2) {
+      System.out
+          .println("Usage: java MultiThreadChatClient <host> <portNumber>\n"
+              + "Now using host=" + host + ", portNumber=" + portNumber);
+    } else {
+      host = args[0];
+      portNumber = Integer.valueOf(args[1]).intValue();
+    }
+
+    /*
+     * Deschidem un socket cu host si port-ul oferit . Deschidem input si output streams.
+     */
+    try {
+      clientSocket = new Socket(host, portNumber);
+      inputLine = new BufferedReader(new InputStreamReader(System.in));
+      os = new PrintStream(clientSocket.getOutputStream());
+      is = new DataInputStream(clientSocket.getInputStream());
+    } catch (UnknownHostException e) {
+      System.err.println("Don't know about host " + host);
+    } catch (IOException e) {
+      System.err.println("Couldn't get I/O for the connection to the host "
+          + host);
+    }
+
+    /*
+     * Daca tot a fost initializat actunci trebuie sa scrim careva date 
+     * pe socketul deschis cu portul si vonexiunea data
+     */
+    if (clientSocket != null && os != null && is != null) {
+      try {
+
+        /* creem un fir pentru a citi de pe server. */
+        new Thread(new MultiThreadChatClient()).start();
+        while (!closed) {
+          os.println(inputLine.readLine().trim());
+        }
+        /*
+         * inchidem output stream, inchidem  input stream, inchidem  socket.
+         */
+        os.close();
+        is.close();
+        clientSocket.close();
+      } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+      }
+    }
+  }
+
+  /*
+   * creem un fir pentru a citi de pe server. (non-Javadoc)
+   * 
+   * @see java.lang.Runnable#run()
+   */
+  public void run() {
+    /*
+     * citim de pe socket cit timp nu primit "Bye" de la 
+     * server. cind receptionam inchidem socketul
+     */
+    String responseLine;
+    try {
+      while ((responseLine = is.readLine()) != null) {
+        System.out.println(responseLine);
+        if (responseLine.indexOf("*** Bye") != -1)
+          break;
+      }
+      closed = true;
+    } catch (IOException e) {
+      System.err.println("IOException:  " + e);
+    }
+  }
+}
